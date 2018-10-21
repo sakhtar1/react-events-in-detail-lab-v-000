b486f768b5b4100fef473b56c5ac113cef32b9d3
Sharmina Akhtar <sakht318@gmail.com>
2018-10-21 18:43:37 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -2,15 +2,28 @@
 import React from 'react';
 
 export default class CoordinatesButton extends React.Component {
+<<<<<<< HEAD
 
+=======
+>>>>>>> 4c86d367df7966bd8a2ea2feb5b4fc1a498366e2
   handleClick = (event) => {
     this.props.onReceiveCoordinates([event.clientX, event.clientY]);
   }
 
+<<<<<<< HEAD
 
   render(){
     return(
+=======
+  render() {
+    return (
+>>>>>>> 4c86d367df7966bd8a2ea2feb5b4fc1a498366e2
       <button onClick={this.handleClick}>Button</button>
     );
   }
 }
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 4c86d367df7966bd8a2ea2feb5b4fc1a498366e2
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -4,16 +4,24 @@ import React from 'react';
 export default class DelayedButton extends React.Component{
 
   handleClick = (event) => {
+<<<<<<< HEAD
     event.persist();
     setTimeout(() => {
       this.props.onDelayedClick(event);
     }, this.props.delay);
+=======
+    this.props.onDelayedClick(event.setTimeout[this.props.delay]);
+>>>>>>> 4c86d367df7966bd8a2ea2feb5b4fc1a498366e2
   }
 
   render() {
 
     return(
+<<<<<<< HEAD
       <button onClick={this.handleClick}>Button</button>
+=======
+      <button onclick={this.handleClick}> Button </button>
+>>>>>>> 4c86d367df7966bd8a2ea2feb5b4fc1a498366e2
     );
 
 
